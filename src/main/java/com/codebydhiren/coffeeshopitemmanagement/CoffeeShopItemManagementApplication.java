16af6f0373d3f1af7b9288d4c35d1c8626cadbc9
Dhiren Kumar <dhirenwipro@gmail.com>
2019-06-14 21:49:33 +0530

    initial commit

---
+++ src/main/java/com/codebydhiren/coffeeshopitemmanagement/CoffeeShopItemManagementApplication.java
@@ -0,0 +1,13 @@
+package com.codebydhiren.coffeeshopitemmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeShopItemManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeShopItemManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codebydhiren/coffeeshopitemmanagement/CoffeeShopItemManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codebydhiren.coffeeshopitemmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeShopItemManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
