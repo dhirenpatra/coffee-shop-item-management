6138907f7a915846405da8e0a58e75ab6227e1f3
Dhiren Kumar <dhirenwipro@gmail.com>
2019-06-18 21:55:57 +0530

    product api with functional endpoint created

--- src/main/java/com/codebydhiren/coffeeshopitemmanagement/router/RouterConfig.java
+++ src/main/java/com/codebydhiren/coffeeshopitemmanagement/router/RouterConfig.java
@@ -8,15 +8,26 @@ import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 
 @Configuration
 public class RouterConfig {
 
+    /**
+     * Order of Routes are impo
+     *
+     * @param productHandler
+     * @return
+     */
     @Bean
-    public RouterFunction<ServerResponse> route(ProductHandler productHandler) {
-        return RouterFunctions.route(GET("/products/functional").and(accept(MediaType.APPLICATION_JSON)) , productHandler :: getAllProducts);
+    public RouterFunction<ServerResponse> routes(ProductHandler productHandler) {
+        return RouterFunctions.route(GET("/products/functional").and(accept(MediaType.APPLICATION_JSON)) , productHandler :: getAllProducts)
+                .andRoute(POST("/products/functional").and(accept(MediaType.APPLICATION_JSON)) , productHandler :: addProduct)
+                .andRoute(DELETE("/products/functional").and(accept(MediaType.APPLICATION_JSON)) , productHandler :: deleteAllProduct)
+                .andRoute(GET("/products/functional/event").and(accept(MediaType.TEXT_EVENT_STREAM)) , productHandler :: productEvents)
+                .andRoute(GET("/products/functional/{id}").and(accept(MediaType.APPLICATION_JSON)) , productHandler :: getSpecificProduct)
+                .andRoute(DELETE("/products/functional/{id}").and(accept(MediaType.APPLICATION_JSON)) , productHandler :: deleteProduct)
+                .andRoute(PUT("/products/functional/{id}").and(accept(MediaType.APPLICATION_JSON)) , productHandler :: updateProduct);
     }
 
 }
